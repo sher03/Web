5029f2efa136796402483b6e7caa060811df5e6f
unknown <sherhan452@gmail.com>
2018-11-23 12:48:18 +0600

    askljf;ja

---
+++ MyJob4/SJava/SJava.js
@@ -0,0 +1,6 @@
+
+		document.getElementById("demo").innerHTML="helo";
+		document.getElementById("imge").src="Jimg/ny.jpg";
+		document.getElementById("Htext").innerHTML="MY WEBSITE LOGO";
+		document.getElementById("pAbout").innerHTML="<i>I love photography</i>";
+		document.getElementById("myName").src="Jimg/avatar.jpg";
